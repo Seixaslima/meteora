0b7cfbb6bc306ebc8d053d41b30498e4c5170fd3
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-26 19:14:01 -0300

    implementação completa do rodape

--- src/componentes/Rodape/Rodape.jsx
+++ src/componentes/Rodape/Rodape.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <div>rodape</div>
+      <p className={styles.texto}>2023 © Desenvolvido por Alura | </p>
+      <p className={styles.texto}>Projeto fictício sem fins comerciais.</p>
     </footer>
   );
 }
