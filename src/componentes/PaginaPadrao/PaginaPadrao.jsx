3fe7eb59c9565a0ac3bf5f1b8249b846549b01a4
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-26 18:59:19 -0300

    começo da implementação do rodape

--- src/componentes/PaginaPadrao/PaginaPadrao.jsx
+++ src/componentes/PaginaPadrao/PaginaPadrao.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 // import styles from './PaginaPadrao.module.css';
 
 import Cabecalho from 'componentes/Cabecalho/Cabecalho';
+import Rodape from 'componentes/Rodape/Rodape';
 
 export default function PaginaPadrao({ children }) {
   return (
@@ -11,6 +12,7 @@ export default function PaginaPadrao({ children }) {
         <Outlet />
         {children}
       </main>
+      <Rodape />
     </>
   );
 }
---
+++ src/componentes/Rodape/Rodape.jsx
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+import React from 'react';
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <div>rodape</div>
+    </footer>
+  );
+}
