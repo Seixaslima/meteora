98237a6d2edad7d14f90bd01287ffd41c2f19e28
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-29 10:59:06 -0300

    correcao estilo do modal produto

--- src/componentes/CardProduto/ModalProduto/ModalProduto.jsx
+++ src/componentes/CardProduto/ModalProduto/ModalProduto.jsx
@@ -18,79 +18,81 @@ export default function ModalProduto({ produto, dispositivo }) {
           />
         </Col>
         <Col sm={6} xs={12}>
-          <div className={styles.cabecalho}>
-            <h5>{produto.produto}</h5>
-            <p>{produto.descricao}</p>
+          <div className={styles.info}>
+            <div className={styles.cabecalho}>
+              <h5>{produto.produto}</h5>
+              <p>{produto.descricao}</p>
+            </div>
+            <div className={styles.precoContainer}>
+              <div className={styles.preco}>R$ {produto.valor}.00</div>
+              <p>Vendido e entregue por Riachuelo</p>
+            </div>
+            <form onSubmit={evento => evento.preventDefault()}>
+              <fieldset className={styles.coresContainer}>
+                <legend className={styles.caracteristicas}>Cores:</legend>
+                <div className={styles.botoesSelecao}>
+                  <div className={styles.botaoSelecao}>
+                    <input
+                      type="radio"
+                      name="cor"
+                      id="AzulClaro"
+                      value="Azul claro"
+                      style={{ backgroundColor: '#75D7F0' }}
+                    />
+                    <label htmlFor="AzulClaro">Azul claro</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input
+                      type="radio"
+                      name="cor"
+                      id="Preto"
+                      value="Preto"
+                      style={{ backgroundColor: 'black' }}
+                    />
+                    <label htmlFor="Preto">Preto</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input
+                      type="radio"
+                      name="cor"
+                      id="Offwhite"
+                      value="Offwhite"
+                      style={{ backgroundColor: '#F3F6F7' }}
+                    />
+                    <label htmlFor="AzulClaro">Offwhite</label>
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset className={styles.tamanhoContainer}>
+                <legend className={styles.caracteristicas}>Tamanhos:</legend>
+                <div className={styles.botoesSelecao}>
+                  <div className={styles.botaoSelecao}>
+                    <input type="radio" name="Tamanho" id="PP" value="PP" />
+                    <label htmlFor="PP">PP</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input type="radio" name="Tamanho" id="P" value="P" />
+                    <label htmlFor="P">P</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input type="radio" name="Tamanho" id="M" value="M" />
+                    <label htmlFor="M">M</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input type="radio" name="Tamanho" id="G" value="G" />
+                    <label htmlFor="G">G</label>
+                  </div>
+                  <div className={styles.botaoSelecao}>
+                    <input type="radio" name="Tamanho" id="GG" value="GG" />
+                    <label htmlFor="GG">GG</label>
+                  </div>
+                </div>
+              </fieldset>
+              <button className={styles.botaoSubmit} type="submit">
+                Adicionar à sacola
+              </button>
+            </form>
           </div>
-          <div className={styles.precoContainer}>
-            <div className={styles.preco}>R$ {produto.valor}.00</div>
-            <p>Vendido e entregue por Riachuelo</p>
-          </div>
-          <form onSubmit={evento => evento.preventDefault()}>
-            <fieldset className={styles.coresContainer}>
-              <legend className={styles.caracteristicas}>Cores:</legend>
-              <div className={styles.botoesSelecao}>
-                <div className={styles.botaoSelecao}>
-                  <input
-                    type="radio"
-                    name="cor"
-                    id="AzulClaro"
-                    value="Azul claro"
-                    style={{ backgroundColor: '#75D7F0' }}
-                  />
-                  <label htmlFor="AzulClaro">Azul claro</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input
-                    type="radio"
-                    name="cor"
-                    id="Preto"
-                    value="Preto"
-                    style={{ backgroundColor: 'black' }}
-                  />
-                  <label htmlFor="Preto">Preto</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input
-                    type="radio"
-                    name="cor"
-                    id="Offwhite"
-                    value="Offwhite"
-                    style={{ backgroundColor: '#F3F6F7' }}
-                  />
-                  <label htmlFor="AzulClaro">Offwhite</label>
-                </div>
-              </div>
-            </fieldset>
-            <fieldset className={styles.tamanhoContainer}>
-              <legend className={styles.caracteristicas}>Tamanhos:</legend>
-              <div className={styles.botoesSelecao}>
-                <div className={styles.botaoSelecao}>
-                  <input type="radio" name="Tamanho" id="PP" value="PP" />
-                  <label htmlFor="PP">PP</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input type="radio" name="Tamanho" id="P" value="P" />
-                  <label htmlFor="P">P</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input type="radio" name="Tamanho" id="M" value="M" />
-                  <label htmlFor="M">M</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input type="radio" name="Tamanho" id="G" value="G" />
-                  <label htmlFor="G">G</label>
-                </div>
-                <div className={styles.botaoSelecao}>
-                  <input type="radio" name="Tamanho" id="GG" value="GG" />
-                  <label htmlFor="GG">GG</label>
-                </div>
-              </div>
-            </fieldset>
-            <button className={styles.botaoSubmit} type="submit">
-              Adicionar à sacola
-            </button>
-          </form>
         </Col>
       </Row>
     </Container>
