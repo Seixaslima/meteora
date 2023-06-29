70678a6960f5ba57755f4b7b28e6b1b15840c62b
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-29 12:41:09 -0300

    texte do opengraph

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import Inicial from 'Paginas/Inicial/Inicial';
+import Opengraph from 'componentes/Opengraph/Opengraph';
 import PaginaPadrao from 'componentes/PaginaPadrao/PaginaPadrao';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicial />} />
         </Route>
+        <Route path="/opengraph" element={<Opengraph />} />
       </Routes>
     </BrowserRouter>
   );
