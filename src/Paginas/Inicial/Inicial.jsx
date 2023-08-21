aa43b1adcb25f39631fc784b7596887e3deadb81
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-08-21 11:19:16 -0300

    correcão de anuncio

--- src/Paginas/Inicial/Inicial.jsx
+++ src/Paginas/Inicial/Inicial.jsx
@@ -15,6 +15,21 @@ export default function Inicial() {
       <SecaoProdutos />
       <Facilidades />
       <Newsletter />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1129823157320382"
+        crossorigin="anonymous"
+      ></script>
+      {/* <!-- Display --> */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1129823157320382"
+        data-ad-slot="2444726825"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </>
   );
 }
--- src/Paginas/PaginaBusca/PaginaBusca.jsx
+++ src/Paginas/PaginaBusca/PaginaBusca.jsx
@@ -12,6 +12,21 @@ export default function PaginaBusca() {
         itemFiltrado={parametros.busca}
         tipoFiltro="produto"
       />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1129823157320382"
+        crossorigin="anonymous"
+      ></script>
+      {/* <!-- Display --> */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1129823157320382"
+        data-ad-slot="2444726825"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </>
   );
 }
